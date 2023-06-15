4178f0f8214aac9a116a3cb8fa0881194fa04b0d
s1300100 <s1300100@u-aizu.ac.jp>
2023-06-15 14:34:09 +0900

    Greetings added

--- Dice_Game.java
+++ Dice_Game.java
@@ -6,16 +6,8 @@ class Dice_Game {
 
     public void run() {
        
-        System.out.println("Rolling dice...");
-        int d1, d2;
-        d1 = new java.util.Random().nextInt(6);
-        d1++;
-        d2 = new java.util.Random().nextInt(6);
-        d2++;
-        System.out.println("Die 1: " + d1);
-        System.out.println("Die 2: " + d2);
-        int sum = d1 + d2;
-        System.out.println("Total value: " + sum);
-       
+        System.out.println("What is your name?");
+        String name = new java.util.Scanner(System.in).nextLine();
+        System.out.println("Hello, " + name + "!");
     }
 }
