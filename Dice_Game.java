678ddf9854f5ae1752c5a96d6f9be42a485e2938
s1300100 <s1300100@u-aizu.ac.jp>
2023-06-15 14:47:53 +0900

    Won/lost message added

--- Dice_Game.java
+++ Dice_Game.java
@@ -5,9 +5,7 @@ class Dice_Game {
     }
 
     public void run() {
-        System.out.println("What is your name?");
-        String name = new java.util.Scanner(System.in).nextLine();
-        System.out.println("Hello, " + name + "!");
+        
         System.out.println("Rolling dice...");
         int d1, d2;
         d1 = new java.util.Random().nextInt(6);
@@ -18,7 +16,7 @@ class Dice_Game {
         System.out.println("Die 2: " + d2);
         int sum = d1 + d2;
         System.out.println("Total value: " + sum);
-        if(sum > 7) System.out.println( name + " won!");
-        else System.out.println(name + " lost!");
+        if(sum > 7) System.out.println( "You won!");
+        else System.out.println( "You lost!");
     }
 }
